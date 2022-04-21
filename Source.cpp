bd1e3712cce928a37a73db9afebc52410a99831e
Explynex <96521396+Explynex@users.noreply.github.com>
2022-04-21 18:37:10 +0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,11 @@
 #include "devattribute.h"
 
-BOOL ShowConsoleCursor(BOOL bShow)
-{
-    CONSOLE_CURSOR_INFO cci;
-    HANDLE hStdOut;
-    hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (hStdOut == INVALID_HANDLE_VALUE)
-        return FALSE;
-    if (!GetConsoleCursorInfo(hStdOut, &cci))
-        return FALSE;
-    cci.bVisible = bShow;
-    if (!SetConsoleCursorInfo(hStdOut, &cci))
-        return FALSE;
-    return TRUE;
+bool showConsoleCursor(bool bShow){
+	CONSOLE_CURSOR_INFO cci;
+	if (!GetConsoleCursorInfo(hStdOut, &cci)) return 0;
+	cci.bVisible = bShow;
+	if (!SetConsoleCursorInfo(hStdOut, &cci)) return 0;
+	return 1;
 }
 
 int main() {
@@ -22,9 +15,9 @@ int main() {
     bool condition = 0;
     int time;
     sheetGenerator(0, 0, 1, 1, 40, 5);
-    GotoXY(10, 1, "Таймер выключения ПК",BROWN,BLACK);
-    GotoXY(2, 3, "Время до завершения работы: ");
-    GotoXY(2, 4, "/s - сек. /m - мин.:",DARKGRAY,BLACK);
+    GotoXY(10, 1, "РўР°Р№РјРµСЂ РІС‹РєР»СЋС‡РµРЅРёСЏ РџРљ",BROWN,BLACK);
+    GotoXY(2, 3, "Р’СЂРµРјСЏ РґРѕ Р·Р°РІРµСЂС€РµРЅРёСЏ СЂР°Р±РѕС‚С‹: ");
+    GotoXY(2, 4, "/s - СЃРµРє. /m - РјРёРЅ.:",DARKGRAY,BLACK);
     do {
         cleaning(30, 3, 1, 7);
         GotoXY(30, 3);
@@ -36,24 +29,20 @@ int main() {
     } while (info != "/s" && info != "/m");
     GotoXY(33, 3);
     time = stoi(printFilter(33, 3, 5, "digit", "0", "9", 1));
-    ShowConsoleCursor(FALSE);
+    showConsoleCursor(false);
     if (info == "/m") time *= 60;
     command = "shutdown /s /t " + std::to_string(time) + " /f";
     system(data(command));
-    GotoXY(2, 3, "Осталось до выключения:            ");
-    GotoXY(2, 4, "Чтобы отменить выключение нажмите ESC",DARKGRAY,BLACK);
+    GotoXY(2, 3, "РћСЃС‚Р°Р»РѕСЃСЊ РґРѕ РІС‹РєР»СЋС‡РµРЅРёСЏ:            ");
+    GotoXY(2, 4, "Р§С‚РѕР±С‹ РѕС‚РјРµРЅРёС‚СЊ РІС‹РєР»СЋС‡РµРЅРёРµ РЅР°Р¶РјРёС‚Рµ ESC",DARKGRAY,BLACK);
     for (; 0 <= time; time--) {
-        GotoXY(27, 3, std::to_string(time)+" с    ",LIGHTRED,BLACK);
+        GotoXY(27, 3, std::to_string(time)+" СЃ    ",LIGHTRED,BLACK);
         if (GetAsyncKeyState(VK_ESCAPE) != 0) {
             system("shutdown /a");
-            GotoXY(26, 3, "Отменено!     ", LIGHTRED, BLACK);
+            GotoXY(26, 3, "РћС‚РјРµРЅРµРЅРѕ!     ", LIGHTRED, BLACK);
             break;
         }
         Sleep(999);
     }
     Sleep(1500);
 }
-
-
-//system("mode con cols=60 lines=4");
-//system("shutdown /s /t 10 /c \"Вы долбоеб! Мои поздравления!\" /f");
